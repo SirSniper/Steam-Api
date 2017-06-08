aba71de890cb505f964635a0159ce5bd230e3895
SirSniper <nikolas.blain@gmail.com>
2017-06-08 14:20:04 -0400

    Add files via upload

--- steam_market.py
+++ steam_market.py
@@ -1,30 +1,13 @@
-import requests, urllib
+"""
+Lasted Edited on May 21, 2017
+"""
 
-'''
-TODO:
-  * add all currencies supported by the Steam Marketplace to `curAbbrev`
-  * create docstrings for all functions
-  * listings parser; get total number of listings (`total_count` in JSON)
-  * get price overview via http://steamcommunity.com/market/priceoverview/
-'''
+import requests, urllib, locale
 
-class MarketListing:
-    def __init__(self, listing_id, price):
-        self._id = listing_id
-        self.price = price
-
-class MarketItem:
-    def __init__(self, item_id=None, listings=None):
-        self._id = item_id
-        if listings:
-            self.listings = listings
-        else:
-            self.listings = []
-
-    def add_listing(self, l_id, price):
-        self.listings.append(MarketListing(l_id, price))
-
-# Currency abbreviations
+"""
+Valid currency codes
+--------------------
+"""
 curAbbrev = {
     'USD' : 1,
     'GBP' : 2,
@@ -36,51 +19,109 @@ curAbbrev = {
 }
 
 """
-Gets item listings from the Steam Marketplace.
-
-@param game_id: ID of game item belongs to.
-@param item: Name of item to lookup.
-@param start: Listing index to start from. 0 is the most recent listing.
-@param start: number >= 0
-@param count: Number of listings to grab, start and beyond.
-@param count: number >= 1
-@param currency: Abbreviation of currency to return listing prices in.
-@type currency:
-    Accepted currencies:
-
-      - USD
-      - GBP
-      - EUR
-      - CHF
-      - RUB
-      - KRW
-      - CAD
-
-    Please lookup the proper abbreviation for your currency of choice.
-@return A list of prices. Depending on your chosen currency, you may need to
-    move the decimal place. For instance, in USD, $25.98 would be returned
-    as 2598
+Formats the varying prices from Steam's API as they are formatted based on locale and currency
+Removes the currency symbol and sets the locale to convert the currency to a float
 """
-def get_item(game_id, item, start=0, count=10, currency='USD'):
-    url = 'http://steamcommunity.com/market/listings/{}/{}/render'.format(
-        game_id,
-        urllib.parse.quote(item)
-    )
-    payload = {
-        'start' : start,
-        'count' : count,
-        'currency' : curAbbrev[currency]
-    }
-    resp = requests.get(url, params=payload)
-    listings = resp.json()['listinginfo']
-    market_item = MarketItem()
-    for l_id, v in listings.items():
-        price = v['converted_price_per_unit'] + v['converted_fee_per_unit']
-        market_item.add_listing(l_id, price)
-    return market_item
+def format_price(price_string, currency):
+    formatted_string = price_string
+    if(currency == 'USD'):
+        formatted_string = formatted_string.replace('$','')
+        locale.setlocale(locale.LC_NUMERIC, 'en-US')
+    elif(currency == 'GBP'):
+        formatted_string = formatted_string.replace('\u00a3','')
+        locale.setlocale(locale.LC_NUMERIC, 'en-GB')
+    elif(currency == 'EUR'):
+        formatted_string = formatted_string.replace('\u20ac','')
+        locale.setlocale(locale.LC_NUMERIC, 'fr-FR')
+    elif(currency == 'CHF'):
+        formatted_string = formatted_string.replace('CHF ','')
+        locale.setlocale(locale.LC_NUMERIC, 'en-US')
+    elif(currency == 'RUB'):
+        formatted_string = formatted_string.replace(' p\u0443\u0431.','')
+        locale.setlocale(locale.LC_NUMERIC, 'ru-RU')
+    elif(currency == 'KRW'):
+        formatted_string = formatted_string.replace('\u20a9 ','')
+        locale.setlocale(locale.LC_NUMERIC, 'ko-KR')
+    elif(currency == 'CAD'):
+        formatted_string = formatted_string.replace(' p\u0443\u0431.','')
+        locale.setlocale(locale.LC_NUMERIC, 'en-CA')
+    return locale.atof(formatted_string)
+    
+"""
+Gets item data from the steam market.
 
-def get_tf2_item(item, start=0, count=10, currency='USD'):
-    return get_item('440', item, start, count)
+@param name: the full name of the steam market item
+@param appid: the steam id for the game you are utilizing (for instance Counter Strike: Global Offensive is 730)
+@param currency: the currency code for the item you are requesting from the following list: USD, GBP, EUR, CHF, RUB, KRW, CAD
 
-def get_csgo_item(item, start=0, count=10, currency='USD'):
-    return get_item('730', item, start, count)
+get_lowest_price
+@return: the lowest price of the item
+
+get_volume
+@return: the number of item listings
+
+get_median_price
+@return: the average price of the item listings
+"""
+class MarketItem:
+    def __init__(self, name, appid, currency):
+        self.name = name
+        self.appid = appid
+        self.currency = currency
+        self.get_item(appid, currency, name)
+        
+    def get_item(self, app_id, currency, name):
+        url = 'http://steamcommunity.com/market/priceoverview/?'
+        #Checks to see if the currency is valid and raises and exception if not
+        if(not(curAbbrev.__contains__(currency))):
+            raise NameError('Invalid Currency Code, should be one of: USD, GBP, EUR, CHF, RUB, KRW, CAD')
+        #Payload for http get request
+        data = { 'appid' : self.appid,
+            'currency' : curAbbrev[self.currency],
+            'market_hash_name' : self.name
+        }
+        encoded_data = urllib.parse.urlencode(data)
+        encoded_data = encoded_data.encode("utf-8")
+        resp_json = requests.get(url, encoded_data).json()
+        #Checks to see if the request succeeded and fills with obviously false data
+        if(resp_json.__getitem__('success') == 'False'):
+            self.lowest_price = -9999
+            self.volume = 0
+            self.median_price = -9999
+            raise KeyError("Query Failed, ensure that the name and app id are correct")
+        else:
+            self.lowest_price = format_price(resp_json.__getitem__('lowest_price'),currency)
+            self.volume = int(resp_json.__getitem__('volume'))
+            self.median_price = format_price(resp_json.__getitem__('median_price'),currency)
+
+    #Allows user to get the different information for an item
+    def get_lowest_price(self):
+        return self.lowest_price
+    
+    def get_volume(self):
+        return self.volume
+    
+    def get_median_price(self):
+        return self.median_price
+
+"""
+Takes an array of item names from the same app id
+
+Returns a dictionary with the keys as item names and the values as prices in the specified currency
+
+Will run through all the names and will fill failed names with -9999 as the value
+"""    
+def get_lowest_of_each(names, appid, currency):
+    price_dict = {}
+    try:
+        if(len(names) == 0):
+            raise TypeError('Names should contain at least 1 name in list')
+    except Exception as e:
+        raise TypeError('Names should be a list')
+    for name in names:
+        try:
+            item = MarketItem(name, appid, currency)
+            price_dict[name] = item.get_lowest_price()
+        except Exception as e:
+            print('Error getting item')
+    return price_dict
